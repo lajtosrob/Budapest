e692d28645ca11b91a6f13911b8dad676cca27e8
lajtosrob <lajtosrobert@freemail.hu>
2024-01-08 15:55:56 +0100

    Add project files.

---
+++ DataSource/Kozterulet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budapest.DataSource
+{
+    internal class Kozterulet
+    {
+        int irsz;
+        string nev;
+        string utotag;
+        string ker;
+
+        public Kozterulet(int irsz, string nev, string utotag, string ker)
+        {
+            this.irsz = irsz;
+            this.nev = nev;
+            this.utotag = utotag;
+            this.ker = ker;
+        }
+
+        public int Irsz { get => irsz; set => irsz = value; }
+        public string Nev { get => nev; set => nev = value; }
+        public string Utotag { get => utotag; set => utotag = value; }
+        public string Ker { get => ker; set => ker = value; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using Budapest.DataSource;
+
+namespace Budapest
+{
+    internal class Program
+    {
+        static List<Kozterulet> kozteruletek = new List<Kozterulet> ();
+        static void Main(string[] args)
+        {
+            // 1. feladat
+
+            StreamReader sr = new StreamReader(".\\DataSource\\bp.csv");
+
+            sr.ReadLine(); // fejléc sor olvasása
+
+            while (!sr.EndOfStream)
+            {
+                string[] line = sr.ReadLine().Split(";");
+
+                Kozterulet adatsor = new Kozterulet(
+                    int.Parse(line[0]),
+                    line[1],
+                    line[2],
+                    line[3]
+                    );
+
+                kozteruletek.Add(adatsor);
+
+            }
+
+            // 2. feladat 
+
+            Console.WriteLine($"2. feladat: Közterületek száma: {kozteruletek.Count()}");
+
+
+        }
+    }
+}
